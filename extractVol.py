11b44686702be5967c3857fbf78176361be01442
Matthew Rindel <matthew@sobantu.co.za>
2019-02-27 16:13:30 +0200

    * added parsing of volume item structure which defines the offset, size and compression type of a file

--- extractVol.py
+++ extractVol.py
@@ -22,6 +22,8 @@ for importFilename in importFilenames:
 	offset = 0
 	headerFmt = "<4sL"
 	fileFmt = "<4sL"
+	itemHeaderFmt = "<4sL"
+	itemFmt = "<4LB"
 	footerFmt = "<4sL4sL4sL"
 	(header, totalFileLength) = struct.unpack_from(headerFmt, rawData, offset)
 	print (header, totalFileLength)
@@ -30,10 +32,18 @@ for importFilename in importFilenames:
 	offset += struct.calcsize(footerFmt)
 	fileListEndIndex = offset + footer[-1]
 	filenames = []
+	fileInfo = []
 	while offset < fileListEndIndex:
 		endIndex = rawData.index("\0", offset)
 		filenames.append(rawData[offset:endIndex])
 		offset += (endIndex - offset + 1)
+
+	offset = rawData.index("voli", offset)
+	itemsHeader = struct.unpack_from(itemHeaderFmt, rawData, offset)
+	offset += struct.calcsize(itemHeaderFmt) 
+	for i in range(len(filenames)):
+		print struct.unpack_from(itemFmt, rawData, offset)	
+		offset += struct.calcsize(itemFmt) 
 	offset = 0
 	files = []
 	fileIndex = 0
@@ -50,9 +60,10 @@ for importFilename in importFilenames:
 		rawFileheader = rawFileheader[:-1]  +  "\0"
 		(fileHeader, fileLength) = struct.unpack(fileFmt, rawFileheader)
 		offset += struct.calcsize(fileFmt)
-		with open(destDir + "/" + filenames[fileIndex],"w") as shapeFile:
-		        newFileByteArray = bytearray(rawData[offset:offset + fileLength])
-		        shapeFile.write(newFileByteArray)
+		print (filenames[fileIndex], fileLength)
+	#	with open(destDir + "/" + filenames[fileIndex],"w") as shapeFile:
+	#	        newFileByteArray = bytearray(rawData[offset:offset + fileLength])
+	#	        shapeFile.write(newFileByteArray)
 		fileIndex += 1
 
 
