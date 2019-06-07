b295dc71f41a9edb6193913fdf79d32e02d16677
Matthew Rindel <matthew@sobantu.co.za>
2019-06-07 15:19:59 +0200

    Moved string constants to top of file
    Added verification check for file length

--- extract-vol.py
+++ extract-vol.py
@@ -14,6 +14,12 @@ compressionTypes = {
 
 FileInfo = namedtuple("FileInfo", "compression_type start_offset end_offset length filename")
 
+vol = "VOL".encode("utf-8")
+vols = "vols".encode("utf-8")
+voli = "voli".encode("utf-8")
+vblk = "VBLK".encode("utf-8")
+eos = "\0".encode("utf-8")
+
 
 def git_file_list_offsets(raw_data):
     offset = 0
@@ -22,13 +28,13 @@ def git_file_list_offsets(raw_data):
     footer_fmt = "<4sL4sL4sL"
     (header, lengthUntilFooter) = struct.unpack_from(header_fmt, raw_data, offset)
 
-    if "VOL".encode("utf-8") not in header:
+    if vol not in header:
         raise ValueError("File header is not VOL as expected")
 
     offset = lengthUntilFooter
     footer = struct.unpack_from(footer_fmt, raw_data, offset)
 
-    if footer[0] != "vols".encode("utf-8"):
+    if footer[0] != vols:
         raise ValueError("vols section not found")
 
     offset += struct.calcsize(footer_fmt)
@@ -37,12 +43,12 @@ def git_file_list_offsets(raw_data):
     return offset, file_list_end_index
 
 
-def get_file_names(rawData):
-    (offset, fileListEndIndex) = git_file_list_offsets(rawData)
+def get_file_names(raw_data):
+    (offset, fileListEndIndex) = git_file_list_offsets(raw_data)
     filenames = []
     while offset < fileListEndIndex:
-        end_index = rawData.index("\0".encode("utf-8"), offset)
-        filenames.append(rawData[offset:end_index])
+        end_index = raw_data.index(eos, offset)
+        filenames.append(raw_data[offset:end_index].decode("utf-8"))
         offset += (end_index - offset + 1)
     return filenames, offset
 
@@ -52,10 +58,10 @@ def get_file_metadata(raw_data):
     item_header_fmt = "<4sL"
     item_fmt = "<4LB"
     file_info = []
-    offset = raw_data.index("voli".encode("utf-8"), offset)
+    offset = raw_data.index(voli, offset)
     items_header = struct.unpack_from(item_header_fmt, raw_data, offset)
 
-    if items_header[0] != "voli".encode("utf-8"):
+    if items_header[0] != voli:
         raise ValueError("voli section not found")
 
     offset += struct.calcsize(item_header_fmt)
@@ -72,12 +78,19 @@ def write_files(raw_data, dest_dir, file_info, filenames):
     fileFmt = "<4s4s"
     for index, info in enumerate(file_info):
         offset = info[2]
+        filename = filenames[index]
         (fileHeader, fileLengthRaw) = struct.unpack_from(fileFmt, raw_data, offset)
-        (fileLength,) = struct.unpack("<L", fileLengthRaw[:-1] + "\0".encode("utf-8"))
+        # turning the random data at the end of the file length into \0 to parse correctly
+        (fileLength,) = struct.unpack("<L", fileLengthRaw[:-1] + eos)
+
+        if (fileLength != info[3]):
+            print("File length mismatch for", filename, "Footer value", info[3], "Header value", fileLength)
+
+
         offset += struct.calcsize(fileFmt)
-        if fileHeader == "VBLK".encode("utf-8"):
-            print("writing " + dest_dir + "/" + filenames[index].decode("utf-8"))
-            with open(dest_dir + "/" + filenames[index].decode("utf-8"), "wb") as shapeFile:
+        if fileHeader == vblk:
+            print("writing " + dest_dir + "/" + filename)
+            with open(dest_dir + "/" + filename, "wb") as shapeFile:
                 new_file_byte_array = bytearray(raw_data[offset:offset + info[3]])
                 shapeFile.write(new_file_byte_array)
 
