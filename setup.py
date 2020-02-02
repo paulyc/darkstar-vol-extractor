75c4a364e50d81b15a606b397692b8e27e5d7c82
Matthew <matthew@sobantu.co.za>
2020-02-02 11:16:27 +0200

    Updated package information

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='darkstar-vol-extractor',
     version='0.0.1',
-    packages=[''],
+    packages=find_packages(),
     url='https://github.com/matthew-rindel/darkstar-vol-extractor',
     license='MIT',
     author='Matthew',
