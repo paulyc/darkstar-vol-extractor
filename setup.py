c92198d1cb77e8f86e4633849768ad8fdd5677bc
Matthew <matthew@sobantu.co.za>
2020-02-11 17:38:30 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 print(find_packages())
 setup(
     name='darkstar-vol-extractor',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     url='https://github.com/matthew-rindel/darkstar-vol-extractor',
     license='MIT',
